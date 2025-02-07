9cbb652e068b904e5c2ed9d441411c30e74ddd63
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-02-08 02:47:03 +0500

    main

--- src/app/api/cart.tsx
+++ src/app/api/cart.tsx
@@ -1,6 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import api from '@/utils/woocommerce'; // WooCommerce API instance
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const response = await api.get('/cart'); // Replace with your WooCommerce cart endpoint
     res.status(200).json({ cart: response.data });
